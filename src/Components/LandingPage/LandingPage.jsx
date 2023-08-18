5a1c114919456f0de7b3a5236e240df4442ff6c6
Folayimi <ridwanfolayimi@gmail.com>
2023-08-18 02:58:33 +0100

    created the second section

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,12 +1,81 @@
 import "./LandingPage.css";
 import HeroSection from "../HeroSection/HeroSection";
+import w1 from "../../assets/w1.svg";
+import w2 from "../../assets/w2.svg";
+import w3 from "../../assets/w3.svg";
+import w4 from "../../assets/w4.svg";
+import w5 from "../../assets/w5.svg";
+import w6 from "../../assets/w6.svg";
 const LandingPage = () => {
+  const Reasons = [
+    {
+      image: w1,
+      topic: "Digital Marketing",
+      content:
+        "the quick fox jumps over the lazy dog the quick fox jumps over the lazy dog",
+    },
+    {
+      image: w2,
+      topic: "Digital Marketing",
+      content:
+        "the quick fox jumps over the lazy dog the quick fox jumps over the lazy dog",
+    },
+    {
+      image: w3,
+      topic: "Business Growing",
+      content:
+        "the quick fox jumps over the lazy dog the quick fox jumps over the lazy dog",
+    },
+    {
+      image: w4,
+      topic: "National top 50 firms",
+      content:
+        "the quick fox jumps over the lazy dog the quick fox jumps over the lazy dog",
+    },
+    {
+      image: w5,
+      topic: "National top 50 firms",
+      content:
+        "the quick fox jumps over the lazy dog the quick fox jumps over the lazy dog",
+    },
+    {
+      image: w6,
+      topic: "Web Development",
+      content:
+        "the quick fox jumps over the lazy dog the quick fox jumps over the lazy dog",
+    },
+  ];
   return (
     <>
-      <div className="container">        
+      <div className="container">
         <HeroSection />
         <div className="section1">
-            
+          <div className="topic1">
+            <h1>WHY CHOOSE US</h1>
+            <p>
+              Problems trying to resolve the conflict between the two major
+              realms of Classical physics: Newtonian mechanics{" "}
+            </p>
+          </div>
+          <div className="reasons">
+            {Reasons.map((reason, i) => {
+              return (
+                <>
+                  <div key={i} className="Rcard">
+                    <div className="up">
+                      <div className="image">
+                        <img src={reason.image} alt={`${reason.image}`} />
+                      </div>
+                      <p>{reason.topic}</p>
+                    </div>
+                    <div className="down">
+                      <p>{reason.content}</p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
