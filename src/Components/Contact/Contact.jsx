8c1749f834dd4015d2f4b2a8b16e89ffb70b4362
Folayimi <ridwanfolayimi@gmail.com>
2023-08-20 01:43:51 +0100

    Made some changes

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,11 +7,32 @@ const Contact = () => {
         <div className="overlay">
           <form>
             <div className="topic">
-                <p>Contact Us</p>
-                <h3>Make an Appointment</h3>
+              <p>Contact Us</p>
+              <h3>Make an Appointment</h3>
             </div>
             <div className="form">
-                
+              <div className="wrap">
+                <input type="text" name="fullName" placeholder="Full Name" />
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Example@gmail.com"
+                />
+                <select name="service" id="service">
+                  <option value="select">Please Select</option>
+                </select>
+                <select name="service" id="service">
+                  <option value="select">4:00 Available</option>
+                </select>
+              </div>
+              <textarea
+                name="message"
+                id="message"
+                cols="30"
+                rows="10"
+                placeholder="Message"
+              />
+              <button>Book Appointment</button>
             </div>
           </form>
         </div>
